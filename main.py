c40f7dae21b54bd01fe5858d704bed2af060b84d
QMast <q.mastin@gmail.com>
2025-05-30 16:29:46 -0400

    rotation

--- main.py
+++ main.py
@@ -4,22 +4,25 @@ from player import Player
 
 def main():
     pygame.init()
-    clock = pygame.time.Clock
+    clock = pygame.time.Clock()
     dt = 0
     
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+    
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-
     while True:
+        dt = clock.tick(60)/1000
+       
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
         screen.fill("black")
         player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
-    dt = clock.tick(60)/1000
+        
+    
 
 if __name__ == "__main__":
     main()
